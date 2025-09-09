fd5e38041cd68ef93f7f9558be3bff0e9ca1a4b7
Dhruthi S <dhruthi016@gmail.com>
2025-09-10 01:37:06 +0530

    Update payroll.go

--- backend/models/payroll.go
+++ backend/models/payroll.go
@@ -1,9 +1,10 @@
 package models
 
 type Payroll struct {
-	ID      string  `bson:"_id,omitempty" json:"id"`
-	EmpID   string  `bson:"emp_id" json:"emp_id"`
-	EmpName string  `bson:"emp_name" json:"emp_name"`
-	Salary  float64 `bson:"salary" json:"salary"`
-	Month   string  `bson:"month" json:"month"`
+	ID      string  `bson:"_id,omitempty"`
+	EmpID   string  `bson:"emp_id"`
+	EmpName string  `bson:"emp_name"`
+	Salary  float64 `bson:"salary"`
+	Month   string  `bson:"month"`
 }
+
